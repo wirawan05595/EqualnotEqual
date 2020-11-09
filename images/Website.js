0471280ee57473606908601b696b34dd36ade143
kittiwat <dukes03@users.noreply.github.com>
2020-11-09 20:40:57 +0700

    เพิ่ิมหน้า SVG

---
+++ images/Website.js
@@ -0,0 +1,1 @@
+function fn00000() { document.write( '<embed  width=7680  height=1080  name=Website  src=images/Website.svg  Alt=  type=image/svg-xml  pluginspage=http://www.adobe.com/svg/viewer/install/  wmode=transparent  > '); }
