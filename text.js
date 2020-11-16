3a80133a906d1b7224126c12859ac88762195937
AnitthaJai-on <42957466+AnitthaJai-on@users.noreply.github.com>
2020-11-17 00:26:35 +0700

    Update text.js

--- text.js
+++ text.js
@@ -1,17 +1,18 @@
 document.addEventListener('DOMContentLoaded', function (event) {
     var dataText = ['คุณรู้หรือไม่ ?<br>ไทยติดอันดับ 10 <br>ประเทศที่มีความเหลื่อมล้ำด้าน<br> "ความมั่งคั่ง" มากที่สุดในโลก'
-    , '<br>เป็นปัญหาเรื้อรัง<br>ที่รักษาไม่หายเป็นเวลานาน'
-    , '<br>คนมักพูดกันว่า<br>เหรียญมักมีสองด้านเสมอ', 'เเล้วถ้าเปรียบว่า<br>เหรียญนี้จะกำหนด<br>โชคชะตาชีวิตที่คุณจะได้เกิดมา'
-    , '<br>อยากลองหน่อยมั้ย<br>ว่าคุณจะได้รับมันอย่างไร'];
+    //, '<br>เป็นปัญหาเรื้อรัง<br>ที่รักษาไม่หายเป็นเวลานาน'
+    //, '<br>คนมักพูดกันว่า<br>เหรียญมักมีสองด้านเสมอ', 'เเล้วถ้าเปรียบว่า<br>เหรียญนี้จะกำหนด<br>โชคชะตาชีวิตที่คุณจะได้เกิดมา'
+    //, '<br>อยากลองหน่อยมั้ย<br>ว่าคุณจะได้รับมันอย่างไร'
+];
     function typeWriter(text, i, fnCallback) {
         if (i < (text.length)) {
             document.querySelector("h2").innerHTML = text.substring(0, i + 1) + '<span aria-hidden="true"></span>';
             setTimeout(function () {
                 typeWriter(text, i + 1, fnCallback)
-            }, 100);
+            }, 100);//1 ตัวอักษร : 0.1 วินาที
         }
         else if (typeof fnCallback == 'function') {
-            setTimeout(fnCallback, 700);
+            setTimeout(fnCallback, 700); //ชุดข้อความ สุดท้าย รอ 0.7 วิ
         }
     }
     function StartTextAnimation(i) {
@@ -27,7 +28,7 @@ document.addEventListener('DOMContentLoaded', function (event) {
             typeWriter(dataText[i], 0, function () {
                 setTimeout(function () {
                     StartTextAnimation(i + 1);
-                }, dataText[i].length*40);
+                }, dataText[i].length*40); //จบ 1 ชุดข้อความ จะรอเวลาตาม ความยาวข้อความ * 40
             });
 
         }
